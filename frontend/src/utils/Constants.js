62c2c5a71c57ee00bb821438cab4fbcc54a1e856
sssprahlad <k.sssprahlad@gmail.com>
2025-12-04 19:59:37 +0530

    changes

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,7 +1,7 @@
+import axios from "axios";
 
-import axios from 'axios';
-
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL =
+  "https://ajackus-abuh.onrender.com" || "http://localhost:8080";
 
 export const LOGIN_API = `${API_BASE_URL}/login`;
 export const REGISTER_API = `${API_BASE_URL}/register`;
@@ -12,41 +12,35 @@ export const UPDATE_USER_API = `${API_BASE_URL}/manageUser`;
 export const DELETE_USER_API = `${API_BASE_URL}/manageUser`;
 export const SEARCH_API = `${API_BASE_URL}/searchQuery`;
 
-
-
-
-
-
-
 export const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    withCredentials: true
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            window.location.href = '/login';
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
+    return Promise.reject(error);
+  }
 );
