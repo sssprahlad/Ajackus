7a4096d2f82785e14d224a15e6638932b82720ff
sssprahlad <k.sssprahlad@gmail.com>
2026-01-13 18:10:47 +0530

    updated changes

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -16,7 +16,6 @@ export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-
     Accept: "application/json",
   },
   withCredentials: true,
